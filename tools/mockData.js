1e8dc09fb0bb80f7572681923248675f4ba53272
David Boyne <davidboyne@wk5adm1ns-MacBook-Pro.local>
2014-02-11 11:26:36 +0000

    Updated the mockData.js to include some new sensors

--- tools/mockData.js
+++ tools/mockData.js
@@ -5,7 +5,16 @@ var mockSensors = [
 	{ sensor: 'temperature', maxValue: 30, unit: 'C' },
 	{ sensor: 'distance', maxValue: 10, unit: 'm' },
 	{ sensor: 'light', maxValue: 15, unit: 'Ω' },
-	{ sensor: 'air_quality', maxValue: 10, unit: 'R' }
+	{ sensor: 'air_quality', maxValue: 10, unit: 'R' },
+	{ sensor: 'compass', maxValue: 360, unit: 'd' },
+	{ sensor: 'range1', maxValue: 400, unit: 'cm' },
+	{ sensor: 'range2', maxValue: 400, unit: 'cm' },
+	{ sensor: 'range3', maxValue: 400, unit: 'cm' },
+	{ sensor: 'range4', maxValue: 400, unit: 'cm' },
+	{ sensor: 'range5', maxValue: 400, unit: 'cm' },
+	{ sensor: 'range6', maxValue: 400, unit: 'cm' },
+	{ sensor: 'range7', maxValue: 400, unit: 'cm' },
+	{ sensor: 'range8', maxValue: 400, unit: 'cm' }
 ];
 
 // Create TCP connection
@@ -21,14 +30,13 @@ client.on('end', function() {
 
 // send random streams of data
 function sendRandomData(client) {
-	var time = Math.floor(Math.random() * 2500);
+	var time = Math.floor(Math.random() * 2000);
 
 	setTimeout(function() {
-		var sensor = mockSensors[Math.round(Math.random() * 3)];
+		var sensor = mockSensors[Math.floor(Math.random() * mockSensors.length)];
 		sensor.value = Math.floor(Math.random() * sensor.maxValue);
 
 		client.write(JSON.stringify(sensor, ["sensor", "value", "unit"]) + "\n");
-
 		sendRandomData(client);
 	}, time);
 };
